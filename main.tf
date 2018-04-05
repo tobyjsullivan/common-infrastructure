095243027acce3a9a9e4e2c7b7e8a64fee45b250
Toby Sullivan <tobyjsullivan@gmail.com>
2018-04-05 20:26:22 +1000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "tf_states" {
+  bucket = "terraform-states.tobyjsullivan.com"
+  region = "us-east-1"
+  acl = "private"
+  tags = {
+    name = "Terraform States"
+  }
+}
+
