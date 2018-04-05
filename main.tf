11db88b1e8b20f6fbcd5b35379013efa116c7832
Toby Sullivan <tobyjsullivan@gmail.com>
2018-04-05 21:27:11 +1000

    Set versioning

--- main.tf
+++ main.tf
@@ -6,8 +6,13 @@ resource "aws_s3_bucket" "tf_states" {
   bucket = "terraform-states.tobyjsullivan.com"
   region = "us-east-1"
   acl = "private"
+
   tags = {
     name = "Terraform States"
+  }
+
+  versioning {
+    enabled = true
   }
 }
 
